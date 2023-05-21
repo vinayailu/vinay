b132dc2975e079fa695a450fe41a0f170f65293c
A.VINAY KUMAR <A.VINAY KUMAR@DESKTOP-T2KS3O4>
2023-05-21 22:47:17 +0530

    push

---
+++ parctise/src/test/java/parctise/Testt.java
@@ -0,0 +1,13 @@
+package parctise;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Testt {
+	@Test
+public void test1()
+{
+	WebDriver driver = new ChromeDriver();
+}
+}
